9c04abd238091f82384dc2118aae9a83c3a3ddb0
2akia <2akssultana@gmail.com>
2024-01-12 20:16:30 +0000

    added the questions

--- index.js
+++ index.js
@@ -5,7 +5,53 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the project title:',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of the project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Describe the installation process:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions for use:'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'Apache 2.0', 'BSD 3-Clause', 'None' ]
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Explain how others can contribute to your project:',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide examples and instructions for running tests:',
+      },
+      {
+        type: 'input',
+        name: 'username',
+        message: 'Enter your GitHub username:',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+      },
+      
 ];
 
 // function to write README file
