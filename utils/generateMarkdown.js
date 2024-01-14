d95cc8046e2cf95d6c273d1165eab29a82d1d25a
2akia <2akssultana@gmail.com>
2024-01-14 16:45:08 +0000

    started the generate markedown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,33 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## License
+  ${licenseNotice}
+  
+  ## Contributing
+  ${data.contributing}
+
+  
+
 `;
 }
 
